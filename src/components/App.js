2ae4cd033b6b410187b5a0192228ea4565914a51
pratik kumar <kumarpratik861@gmail.com>
2020-12-11 01:14:20 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,28 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-  
-  const[remainingTime, setRemainingTime] =useState(0);
+  // write your code here
+  const [remainingTime, setRemainingTime] = useState(0);
 
   const handleKeyDown = (event) => {
-    if(event.keyCode ===13){
-      let timeremainnig = event.target.value;
-      if(isNaN(timeremainnig) || timeremainnig <0){
-        timeremainnig =0;
+    if (event.keyCode === 13) {
+      let timeremaining = event.target.value;
+      if (isNaN(timeremaining) || timeremaining < 0) {
+        timeremaining = 0;
       }
-      setRemainingTime(Math.floor(timeremainnig));
+      setRemainingTime(Math.floor(timeremaining));
     }
   };
 
-  document.addEventListener("keydown",(event)=> handleKeyDown(event));
+  document.addEventListener("keydown", (event) => handleKeyDown(event));
 
   useEffect(() => {
-    if (remainingTime > 0){
-      const timerID =setInterval(() => {
-        let timeremainnig =remainingTime -1;
-        setRemainingTime(timeremainnig);
-      },1000);
+    if (remainingTime > 0) {
+      const timerID = setInterval(() => {
+        let timeremaining = remainingTime - 1;
+        setRemainingTime(timeremaining);
+      }, 1000);
       return () => clearInterval(timerID);
     }
   }, [remainingTime]);
@@ -31,13 +31,14 @@ const App = () => {
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={(event) => handleKeyDown(event)} /> sec.
+          Reverse countdown for
+          <input id="timeCount" onKeyDown={(event) => handleKeyDown(event)} />
+          sec.
         </h1>
       </div>
-      <div id="current-time">{remaining time}</div>
+      <div id="current-time">{remainingTime}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
